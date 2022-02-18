46deb73595466f41d60c1d231d87ac6c2b47897c
Rodolfo Lopes <rodolfo.ls1996@outlook.com>
2022-02-18 17:18:32 -0300

    Praticando e criando exceptions

---
+++ src/MinhaException.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Rodolfo
+ *
+ */
+public class MinhaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public MinhaException() {
+		super();
+	}
+	
+	public MinhaException(String msg) {
+		super(msg);
+	}
+	
+	
+}
